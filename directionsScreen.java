94108cfd89fbd8d8d4c1a1e2cb09c025200140ca
ajlent <ajlent@bu.edu>
2016-12-04 17:32:41 -0500

    Create directionsScreen.java

---
+++ directionsScreen.java
@@ -0,0 +1,13 @@
+package com.ec327.android.tuneplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class directionsScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directions_screen);
+    }
+}
