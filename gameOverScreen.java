addae614d99bacf241aeb1b77182f779a1539fac
ajlent <ajlent@bu.edu>
2016-12-10 13:59:49 -0500

    Create gameOverScreen.java

---
+++ gameOverScreen.java
@@ -0,0 +1,24 @@
+package com.ec327.android.tuneplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class gameOverScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_over_screen);
+    }
+
+    // This method is called when the Play Again button is pushed
+    public void playAgain(View view) {
+        // code to take app to next level
+    }
+
+    // This method is called when the Back Home button is pushed
+    public void backHome(View view) {
+        // code to take app to next level
+    }
+}
