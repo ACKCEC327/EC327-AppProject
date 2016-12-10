748c6fa8ac965d8eabb137620ca512b18fc7c34a
ajlent <ajlent@bu.edu>
2016-12-10 13:43:07 -0500

    Create nextLevelScreen.java

---
+++ nextLevelScreen.java
@@ -0,0 +1,19 @@
+package com.ec327.android.tuneplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class nextLevelScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_level_screen);
+    }
+
+    // This method is called when the Next Level button is pushed
+    public void nextLevel(View view) {
+        // code to take app to next level
+    }
+}
