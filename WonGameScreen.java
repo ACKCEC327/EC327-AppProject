ff2ec58c05eef784fd3c6b9e761b8750fb7f71ab
fogarasi <fogarasi@bu.edu>
2016-12-11 17:41:16 -0500

    Add files via upload

---
+++ WonGameScreen.java
@@ -0,0 +1,21 @@
+package com.example.kristof.tuneplay;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WonGameScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_won_game_screen);
+    }
+
+    public void practice(View view){
+        // practice playing the instruments
+        Intent goBackToHome = new Intent(this, HomeScreen.class);
+        startActivity(goBackToHome);
+    }
+}
