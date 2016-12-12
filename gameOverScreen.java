3f95291d5b64744ab9c4b63d7ce02b0aa03b3f3f
fogarasi <fogarasi@bu.edu>
2016-12-11 20:11:44 -0500

    Update gameOverScreen.java

--- gameOverScreen.java
+++ gameOverScreen.java
@@ -1,3 +1,11 @@
+package com.example.kristof.tuneplay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+
+
 public class gameOverScreen extends AppCompatActivity {
 
     @Override
