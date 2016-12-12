5c3c208010aa9853c04997d946c8560f3b5775fd
ajlent <ajlent@bu.edu>
2016-12-11 20:09:50 -0500

    Update gameOverScreen.java

--- gameOverScreen.java
+++ gameOverScreen.java
@@ -1,9 +1,3 @@
-package com.ec327.android.tuneplay;
-
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-
 public class gameOverScreen extends AppCompatActivity {
 
     @Override
@@ -14,11 +8,15 @@ public class gameOverScreen extends AppCompatActivity {
 
     // This method is called when the Play Again button is pushed
     public void playAgain(View view) {
-        // code to take app to next level
+        // code to take app to beginning of game
+        Intent goToGame = new Intent(this, PlayScreen.class);
+        startActivity(goToGame);
     }
 
     // This method is called when the Back Home button is pushed
     public void backHome(View view) {
         // code to take app to next level
+        Intent goToHome = new Intent(this, HomeScreen.class);
+        startActivity(goToHome);
     }
 }
